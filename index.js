3487c6a9b928789baf940a036d60ee4af2187e66
Nati19951995 <134216944+Nati19951995@users.noreply.github.com>
2025-01-15 22:11:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+/*var numbers = [3, 56, 2, 48, 5];             ///1
+var result = numbers.map(function(x) {
+    return x+5;
+
+});
+console.log(result);  
+*/
+
+
+/*
+var Arr = [1,22,33,44,55,2];                  /////2
+
+var Odd = Arr.filter(function(y) {
+    return y%2 ==0;
+
+});
+console.log(Odd);
+*/
+
+
+/*
+const Sumarray = [1,2,3,4];                      //////3
+
+const T = Sumarray.reduce(sum); // פעולת רדיוס
+console.log(T);
+
+ // שני פרמטרים שנלחים שבעצים לי חיבור
+function sum (Pre,next) {
+    return Pre+next;
+}*/
+
+
+/*
+const neww = [1,7,8,311,22,66,55,77,44]; /////////4
+const Twodigit = (values) => {
+let TwoDigitFirst=0;
+    for(let i=0;i<values.length;i++)
+        {
+        if(values[i]>=10 && values[i]<100) // בתחום הדו מספרי
+        {
+            TwoDigitFirst=values[i]; // מצאתי איבר ראשון דו זוגי
+            break;
+        }
+    }
+    return TwoDigitFirst;
+};
+
+console.log(Twodigit(neww));
+*/
+
+/*  
+   /*                    /////5
+
+*/ 
+/*
+const arrayFindMatch = [711,110,330,40,50,60]; ///////// find index 
+const twoDigit = (values) => {
+    let TwoDigitFirst=0;
+    for(let i=0;i<values.length;i++)
+        {
+        if(values[i]>=10 && values[i]<100) // בתחום הדו מספרי
+        {
+            return i; // מצאתי איבר ראשון דו זוגי
+            
+        }
+    }
+     return -1;
+};
+
+console.log(twoDigit(arrayFindMatch));
+*/
+
+
+
+
+
+
+   
